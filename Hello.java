19dbfb8172a3d69a16c6980703521999f69ab98a
jackmad <42514708+yueyang-yuxia@users.noreply.github.com>
2018-08-19 21:35:52 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/*
+Hello.java
+2018-07-19
+v1.0
+*/
+public class Hello{
+        public static void main(String[] args){
+		//输出控制台信息
+		System.out.print("hello world!!");
+		System.out.print("hello world2!!");
+        }
+}
